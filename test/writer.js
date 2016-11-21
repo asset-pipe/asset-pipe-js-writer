fd20609fb8bfba5d132cd8c055df0a5c6ef782f3
Trygve Lie <post@trygve-lie.com>
2016-11-21 15:16:59 +0100

    initial test setup

--- test/writer.js
+++ test/writer.js
@@ -4,9 +4,8 @@ const tap = require('tap');
 const Writer = require('../');
 
 tap.test('foo() - bar', function (t) {
-    var writer = new Writer('./test/mock/main.js');
-
-    writer.bundle().pipe(process.stdout);
+//    var writer = new Writer('./test/mock/main.js');
+//    writer.bundle().pipe(process.stdout);
 
     t.equal(true, true);
     t.end();
