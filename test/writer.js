f08d1a30e2fbd57cb4dbbf0b80f1f0352ea3d1c4
Simen Bekkhus <sbekkhus91@gmail.com>
2017-08-07 18:59:23 +0200

    Fix lint

--- test/writer.js
+++ test/writer.js
@@ -7,8 +7,8 @@ const Writer = require('../');
 tap.test('foo() - bar', (t) => {
     const writer = new Writer('./test/mock/main.js', {}, false, false);
     writer.bundle()
-          .pipe(JSONStream.stringify())
-          .pipe(process.stdout);
+        .pipe(JSONStream.stringify())
+        .pipe(process.stdout);
 
     t.equal(true, true);
     t.end();
