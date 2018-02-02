f06d360d138de68ff8fbc24854b4e38acc24975e
Richard Walker <digitalsadhu@gmail.com>
2018-02-02 10:32:49 +0100

    test: change tap test to jest

--- test/writer.test.js
+++ test/writer.test.js
@@ -1,16 +1,14 @@
 'use strict';
 
 const JSONStream = require('JSONStream');
-const tap = require('tap');
 const Writer = require('../');
 
-tap.test('foo() - bar', t => {
+test('foo() - bar', () => {
     const writer = new Writer('./test/mock/main.js', {}, false, false);
     writer
         .bundle()
         .pipe(JSONStream.stringify())
         .pipe(process.stdout);
 
-    t.equal(true, true);
-    t.end();
+    expect(true).toBe(true);
 });
